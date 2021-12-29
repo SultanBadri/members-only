a9fa2dce52a2782267ff1f1a4cb080d21fe061cc
Sultan Badri <sultanbadri@berkeley.edu>
2021-12-28 17:49:22 -0800

    correctly reverted back

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,9 +4,8 @@ exports.getIndexView = async (req, res, next) => {
   try {
     const messages = await Message.find()
       .sort([["date", "descending"]])
-      .populate("author")
-      .exec();
-    res.render("index", { user: req.user, messages: messages });
+      .populate("author");
+    res.render("index", { user: req.user, messages });
   } catch (err) {
     return next(err);
   }
--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,19 +2,19 @@ const Message = require("../models/messageModel");
 
 exports.getMessage = (req, res) => res.render("message-form");
 
-exports.postMessage = (req, res, next) => {
+exports.postMessage = async (req, res, next) => {
   const { title, content } = req.body;
-  const message = new Message({
-    title: title,
-    content: content,
-    date: Date.now(),
-    author: req.user.id,
-  }).save((err) => {
-    if (err) {
-      return next(err);
-    }
+  try {
+    const message = await new Message({
+      title: title,
+      content: content,
+      date: Date.now(),
+      author: req.user.id,
+    });
     res.redirect("/");
-  });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 exports.deleteMessage = async (req, res, next) => {
